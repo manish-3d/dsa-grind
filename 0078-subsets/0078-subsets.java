aa5115ad24a8f118dc52849430354bdaf8304ebb
Manish Kumar <manishkumar.intc@gmail.com>
2026-08-11 22:29:00 +0530

    Time: 1 ms (86.54%), Space: 44.2 MB (57.68%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -8,13 +8,14 @@ class Solution {
     }
     public void subsetfinder(int[]nums,List<List<Integer>>ans,List<Integer>subans,int index){
         //base case condition 
-        if(index == nums.length){
-            ans.add(new ArrayList<>(subans));
-            return ;
-        }
-        subans.add(nums[index]);
-        subsetfinder(nums,ans,subans,index+1);
-        subans.remove(subans.size()-1);
-        subsetfinder(nums,ans,subans,index+1);
+       if(index == nums.length){
+        ans.add(new ArrayList<>(subans));
+        return;
+       }
+       subans.add(nums[index]);
+       subsetfinder(nums,ans,subans,index+1);
+       subans.remove(subans.size() - 1);
+       subsetfinder(nums,ans,subans,index+1);
+       
     }
 }

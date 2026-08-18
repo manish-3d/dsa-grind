87d65bd917497a724067b382f01872fed1de3a6b
Manish Kumar <manishkumar.intc@gmail.com>
2026-08-18 14:30:28 +0530

    Time: 16 ms (48.01%), Space: 43.8 MB (59.18%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -5,7 +5,6 @@ class Solution {
         HashMap<Character, Character> map2 = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
 

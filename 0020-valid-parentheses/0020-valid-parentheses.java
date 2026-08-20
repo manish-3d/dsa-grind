e9784ff2fe529e54c2f4d15728c9716e14b2346f
Manish Kumar <manishkumar.intc@gmail.com>
2026-08-20 14:29:58 +0530

    Time: 3 ms (86.09%), Space: 43.2 MB (73.43%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+                }else if (stack.isEmpty()){
+                return false;
+                }
+                else if(stack.peek() == '(' && ch == ')' || stack.peek() == '{' && ch == '}' || stack.peek() == '[' && ch ==']'){
+                stack.pop();
+                }else {
+                    return false;
+                }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }   
+}

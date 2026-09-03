6e321e4d4772bddfb10b73f2d9052b8bd2fbc107
Manish Kumar <manishkumar.intc@gmail.com>
2026-09-03 08:46:07 +0530

    Time: 1 ms (84.91%), Space: 42.8 MB (58.15%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,42 +1,29 @@
 class Solution {
     public String decodeString(String s) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        Deque<StringBuilder> ststack = new ArrayDeque<>();
-
-        StringBuilder current = new StringBuilder();
-        int count = 0;
-
-        for (char ch : s.toCharArray()) {
-
-            if (Character.isDigit(ch)) {
-                count = count * 10 + (ch - '0');
-            }
-
-            else if (ch == '[') {
-                stack.push(count);
-                ststack.push(current);
-
-                count = 0;
-                current = new StringBuilder();
-            }
-
-            else if (Character.isAlphabetic(ch)) {
-                current.append(ch);
-            }
-
-            else if (ch == ']') {
-                int repeat = stack.pop();
-
-                StringBuilder previous = ststack.pop();
-
-                for (int i = 0; i < repeat; i++) {
-                    previous.append(current);
-                }
-
-                current = previous;
-            }
+       Deque<Integer> stack = new ArrayDeque<>();
+       Deque<StringBuilder> ststack =  new ArrayDeque<>();
+       StringBuilder current = new StringBuilder();
+       int count = 0;
+    for(char ch : s.toCharArray()){
+        if(Character.isDigit(ch)){
+            count = count*10 + ch - '0';
         }
-
-        return current.toString();
+        else if(ch == '['){
+            stack.push(count);
+            count = 0;
+            ststack.push(current);
+            current = new StringBuilder();
+        }else if(Character.isAlphabetic(ch)){
+            current.append(ch);
+        }else if(ch == ']'){
+            int goat = stack.pop();
+            StringBuilder sb = ststack.pop();
+            for(int i = 0 ;i < goat ; i++){
+                sb.append(current);
+            }
+            current = sb;
+        }
+    }
+return current.toString();
     }
 }

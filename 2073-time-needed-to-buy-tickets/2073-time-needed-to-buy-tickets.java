a36b2d11be00b8f4ecb36b90214d6ccad7043db7
Manish Kumar <manishkumar.intc@gmail.com>
2026-09-07 20:49:29 +0530

    Time: 0 ms (100%), Space: 42.8 MB (85.68%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,16 +1,20 @@
 class Solution {
-    public int timeRequiredToBuy(int[] tickets, int k) {
-        int time = 0;
-        int target = tickets[k];
+    public int timeRequiredToBuy(int[] t, int k) {
+        int target = t[k];
+        int ans = 0;
 
-        for (int i = 0; i < tickets.length; i++) {
-            if (i <= k) {
-                time += Math.min(tickets[i], target);
-            } else {
-                time += Math.min(tickets[i], target - 1);
+        for (int i = 0; i < t.length; i++) {
+            if (t[i] > target && i <= k) {
+                ans += target;
+            } else if (t[i] <= target && i <= k) {
+                ans += t[i];
+            } else if (t[i] >= target && i > k) {
+                ans += target - 1;
+            } else if (t[i] < target && i > k) {
+                ans += t[i];
             }
         }
 
-        return time;
+        return ans;
     }
 }
